f98083138eb26be0266bd43c28ee58f1198ca440
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2019-09-29 18:55:17 +0200

    Revert "Use PEM instead of P12"
    This reverts commit 0a781fa2
    
    Signed-off-by: Sherif Abdel-Naby <sherifabdlnaby@gmail.com>

--- setup/setup-certs.sh
+++ setup/setup-certs.sh
@@ -2,35 +2,23 @@
 set -e
 
 OUTPUT_DIR=/secrets/certs
-CA_ZIP=$OUTPUT_DIR/elastic-stack-ca.zip
-CERT_ZIP=$OUTPUT_DIR/elastic-certificates.zip
-
-if ! command -v unzip &>/dev/null; then
-echo "Install unzip if needed..."
-    yum -qy install unzip &> /dev/null
-fi
+CA_FILE=$OUTPUT_DIR/elastic-stack-ca.p12
+CERT_FILE=$OUTPUT_DIR/elastic-certificates.p12
 
 printf "====== Generating Elasticsearch Certifications ======\n"
 printf "=====================================================\n"
-
-if [ -f "$CA_ZIP" ]; then rm $CA_ZIP; fi
-elasticsearch-certutil ca -s --pem --out $CA_ZIP
-unzip $CA_ZIP -d $OUTPUT_DIR &>/dev/null
-mv $OUTPUT_DIR/ca/ca.key $OUTPUT_DIR/elastic-ca.key
-mv $OUTPUT_DIR/ca/ca.crt $OUTPUT_DIR/elastic-ca.crt
-rm -rf $OUTPUT_DIR/ca
-rm -rf $CA_ZIP
-
-
-if [ -f "$CERT_ZIP" ]; then rm $CERT_ZIP; fi
-elasticsearch-certutil cert --silent --pem --out $CERT_ZIP --ca-cert $OUTPUT_DIR/elastic-ca.crt --ca-key $OUTPUT_DIR/elastic-ca.key
-unzip $CERT_ZIP -d $OUTPUT_DIR &>/dev/null
-mv $OUTPUT_DIR/instance/instance.key $OUTPUT_DIR/elastic.key
-mv $OUTPUT_DIR/instance/instance.crt $OUTPUT_DIR/elastic.crt
-rm -rf $OUTPUT_DIR/instance
-rm -rf $CERT_ZIP
-
-
+if [ -f "$CA_FILE" ]; then
+    echo "Removing current Certificate Authority (CA)..."
+    rm $CA_FILE
+fi
+if [ -f "$CERT_FILE" ]; then
+    echo "Removing current Certificate (P12)..."
+    rm $CERT_FILE
+fi
+elasticsearch-certutil ca -s --pass "" --out $CA_FILE
+elasticsearch-certutil cert -s --ca $CA_FILE --ca-pass "" --out $CERT_FILE --pass ""
+printf "Certificate Authority created at $CA_FILE\n"
+printf "Certificate created at $CERT_FILE\n"
 printf "=====================================================\n"
 printf "SSL Certifications generation completed successfully.\n"
 printf "=====================================================\n"
