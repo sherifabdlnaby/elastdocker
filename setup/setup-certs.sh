d4a5dc8fc3b0684d22d74fb71781d6008cb775a1
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2020-04-07 17:13:13 +0200

    Only Install unzip if it is not found + make output cleaner.
    Signed-off-by: Sherif Abdel-Naby <sherifabdlnaby@gmail.com>

--- setup/setup-certs.sh
+++ setup/setup-certs.sh
@@ -7,15 +7,17 @@ ZIP_FILE=$OUTPUT_DIR/certs.zip
 printf "======= Generating Elastic Stack Certificates =======\n"
 printf "=====================================================\n"
 
-printf "Installing Necessary Tools... \n"
-yum install -y -q -e 0 unzip;
+if ! command -v unzip &>/dev/null; then
+    printf "Installing Necessary Tools... \n"
+    yum install -y -q -e 0 unzip;
+fi
 
 printf "Clearing Old Certificates if exits... \n"
 find $OUTPUT_DIR -mindepth 1 -type d -exec rm -rf -- {} +
 rm -f $ZIP_FILE
 
 printf "Generating... \n"
-bin/elasticsearch-certutil cert --silent --pem --in /setup/instances.yml -out $ZIP_FILE;
+bin/elasticsearch-certutil cert --silent --pem --in /setup/instances.yml -out $ZIP_FILE &> /dev/null
 
 printf "Unzipping Certifications... \n"
 unzip -qq $ZIP_FILE -d $OUTPUT_DIR;
