2781b61af146dbb577f48ceaa54b98dc763f395f
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2020-04-07 17:10:17 +0200

    Do not install S3 Repository Plugin by default.
    Signed-off-by: Sherif Abdel-Naby <sherifabdlnaby@gmail.com>

--- setup/keystore.sh
+++ setup/keystore.sh
@@ -18,10 +18,10 @@ echo "Setting bootstrap.password..."
 
 # ----- Setting S3 Secrets
 
-# Setting S3 Access Key
-echo "Setting S3 Access Key..."
-(echo "$AWS_ACCESS_KEY_ID" | elasticsearch-keystore add -x 's3.client.default.access_key')
-
-# Setting S3 Secret Key
-echo "Setting S3 Secret Key..."
-(echo "$AWS_SECRET_ACCESS_KEY" | elasticsearch-keystore add -x 's3.client.default.secret_key')
+## Setting S3 Access Key
+#echo "Setting S3 Access Key..."
+#(echo "$AWS_ACCESS_KEY_ID" | elasticsearch-keystore add -x 's3.client.default.access_key')
+#
+## Setting S3 Secret Key
+#echo "Setting S3 Secret Key..."
+#(echo "$AWS_SECRET_ACCESS_KEY" | elasticsearch-keystore add -x 's3.client.default.secret_key')
