84eb1c5834c6c9dd708a277084196c2bb79147fe
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2022-03-04 00:43:33 +0200

    🚀 Update Stack to Major Version 8.0
    Signed-off-by: Sherif Abdel-Naby <sherifabdlnaby@gmail.com>

--- setup/setup-keystore.sh
+++ setup/setup-keystore.sh
@@ -1,8 +1,12 @@
 # Exit on Error
 set -e
 
-OUTPUT_FILE=/secrets/keystore/elasticsearch.keystore
-NATIVE_FILE=/usr/share/elasticsearch/config/elasticsearch.keystore
+GENERATED_KEYSTORE=/usr/share/elasticsearch/config/elasticsearch.keystore
+OUTPUT_KEYSTORE=/secrets/keystore/elasticsearch.keystore
+
+GENERATED_SERVICE_TOKENS=/usr/share/elasticsearch/config/service_tokens
+OUTPUT_SERVICE_TOKENS=/secrets/service_tokens
+OUTPUT_KIBANA_TOKEN=/secrets/.env.kibana.token
 
 # Password Generate
 PW=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16 ;)
@@ -14,23 +18,47 @@ printf "========== Creating Elasticsearch Keystore ==========\n"
 printf "=====================================================\n"
 elasticsearch-keystore create >> /dev/null
 
-# Setting Secrets
-echo "Elastic password is: $ELASTIC_PASSWORD"
+# Setting Secrets and Bootstrap Password
 sh /setup/keystore.sh
+echo "Elastic Bootstrap Password is: $ELASTIC_PASSWORD"
+
+# Generating Kibana Token
+echo "Generating Kibana Service Token..."
+
+# Delete old token if exists
+/usr/share/elasticsearch/bin/elasticsearch-service-tokens delete elastic/kibana default &> /dev/null || true
+
+# Generate new token
+TOKEN=$(/usr/share/elasticsearch/bin/elasticsearch-service-tokens create elastic/kibana default | cut -d '=' -f2 | tr -d ' ')
+echo "Kibana Service Token is: $TOKEN"
+echo "KIBANA_SERVICE_ACCOUNT_TOKEN: $TOKEN" > $OUTPUT_KIBANA_TOKEN
 
 # Replace current Keystore
-if [ -f "$OUTPUT_FILE" ]; then
+if [ -f "$OUTPUT_KEYSTORE" ]; then
     echo "Remove old elasticsearch.keystore"
-    rm $OUTPUT_FILE
+    rm $OUTPUT_KEYSTORE
 fi
 
 echo "Saving new elasticsearch.keystore"
-mv $NATIVE_FILE $OUTPUT_FILE
-chmod 0644 $OUTPUT_FILE
+mkdir -p "$(dirname $OUTPUT_KEYSTORE)"
+mv $GENERATED_KEYSTORE $OUTPUT_KEYSTORE
+chmod 0644 $OUTPUT_KEYSTORE
+
+# Replace current Service Tokens File
+if [ -f "$OUTPUT_SERVICE_TOKENS" ]; then
+    echo "Remove old service_tokens file"
+    rm $OUTPUT_SERVICE_TOKENS
+fi
+
+echo "Saving new service_tokens file"
+mv $GENERATED_SERVICE_TOKENS $OUTPUT_SERVICE_TOKENS
+chmod 0644 $OUTPUT_SERVICE_TOKENS
 
 printf "======= Keystore setup completed successfully =======\n"
 printf "=====================================================\n"
 printf "Remember to restart the stack, or reload secure settings if changed settings are hot-reloadable.\n"
 printf "About Reloading Settings: https://www.elastic.co/guide/en/elasticsearch/reference/current/secure-settings.html#reloadable-secure-settings\n"
+printf "=====================================================\n"
 printf "Your 'elastic' user password is: $ELASTIC_PASSWORD\n"
+printf "Your Kibana Service Token is: $TOKEN\n"
 printf "=====================================================\n"
